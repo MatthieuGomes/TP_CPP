f779c6231b64af25be844c3d34a7bdc00b38fea9
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-09-20 23:32:03 +0200

    adding cmakelist + first src files (main + basic_import)

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "utils/basic_import.hpp"
+#include "main.hpp"
+
+int main() {
+    std::cout << "Welcome to Pokemon Ensea !" << std::endl;
+    return 0;
+} 
+
---
+++ src/utils/basic_import.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using std::string;
+#include <bits/stdc++.h>
