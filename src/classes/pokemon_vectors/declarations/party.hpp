9ead432c54d671bb60fc078fb2cbb95db395a56d
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-09-21 14:08:29 +0200

    refactor of party class to ensure successful operations by using bool instead of void + documentation refactor

--- src/classes/pokemon_vectors/declarations/party.hpp
+++ src/classes/pokemon_vectors/declarations/party.hpp
@@ -8,12 +8,79 @@
  */
 class Pokemon_Party : public virtual Pokemon_Vector {
 public:
+    // Constructors & Destructor
+    //// Constructors
+    /**
+     * @brief Constructor for the Pokemon_Party class with a Pokemon object.
+     * @param first_pkmn The first Pokemon to be added to the party.
+     * @details This constructor initializes the party with a single Pokemon provided as an argument.
+     * @note The Pokemon is copied to ensure the party has its own instance.
+     * @see Pokemon
+     */
     Pokemon_Party(Pokemon first_pkmn);
+    /**
+     * @brief Constructor for the Pokemon_Party class with a Pokemon ID.
+     * @param first_pkmn_id The ID of the first Pokemon to be added to the party.
+     * @details This constructor initializes the party by creating a new Pokemon instance using the provided ID.
+     * @note The Pokemon is fetched from the Pokedex using its ID.
+     * @see Pokemon
+     * @see Pokedex
+     */
     Pokemon_Party(int first_pkmn_id);
+    ////
+    //// Destructor
+    /**
+     * @brief Destructor for the Pokemon_Party class.
+     * @details Cleans up resources used by the Pokemon_Party object.
+     * @note Currently, this destructor only outputs a message indicating that the Pokemon_Party has been destroyed.
+     */
     ~Pokemon_Party();
+    ////
+    //
+    // Getters & Setters
+    //// Getters
+    /**
+     * @brief Gets the list of Pokemons in the party.
+     * @return A vector of pointers to the Pokemons in the party.
+     * @details This method returns the current list of Pokemons that are part of the party.
+     */
     std::vector<Pokemon*> getPokemons();
-    void addPokemon(Pokemon pkmn);
-    void addPokemon(int pkmn_id);
-    void removePokemon(int index);
+    /**
+     * @brief Gets a Pokemon from the party by its index.
+     * @param index The index of the Pokemon in the party.
+     * @return A pointer to the Pokemon if found, nullptr otherwise.
+     * @details This method checks if the provided index is valid and returns the corresponding Pokemon from the party. If the index is out of range, it returns nullptr and outputs an error message.
+     */
     Pokemon* getPokemon(int index);
+    ////
+    //// Setters
+    /**
+     * @brief Adds a Pokemon to the party.
+     * @param pkmn The Pokemon object to be added to the party.
+     * @return true if the Pokemon was successfully added, false otherwise.
+     * @details This method adds a copy of the provided Pokemon to the party.
+     * @note The Pokemon is copied to ensure the party has its own instance.
+     * @see Pokemon
+     */
+    bool addPokemon(Pokemon pkmn);
+    /**
+     * @brief Adds a Pokemon to the party using its ID.
+     * @param pkmn_id The ID of the Pokemon to be added to the party.
+     * @return true if the Pokemon was successfully added, false otherwise.
+     * @details This method creates a new Pokemon instance using the provided ID and adds it to the party.
+     * @note The Pokemon is fetched from the Pokedex using its ID.
+     * @see Pokemon
+     * @see Pokedex
+     */
+    bool addPokemon(int pkmn_id);
+    /**
+     * @brief Removes a Pokemon from the party by its index.
+     * @param index The index of the Pokemon to be removed from the party.
+     * @return true if the Pokemon was successfully removed, false otherwise.
+     * @details This method checks if the provided index is valid and removes the corresponding Pokemon from the party. If the index is out of range, it returns false and outputs an error message.
+     */
+    bool removePokemon(int index);
+    //// 
+    //
+    
 };
--- src/classes/pokemon_vectors/party.cpp
+++ src/classes/pokemon_vectors/party.cpp
@@ -1,33 +1,27 @@
 #include "declarations/party.hpp"
 
+// Constructors & Destructor
+//// Constructors
 Pokemon_Party::Pokemon_Party(Pokemon first_pkmn) {
     this->pokemons.push_back(new Pokemon(first_pkmn));
 }
 Pokemon_Party::Pokemon_Party(int first_pkmn_id) {
     this->pokemons.push_back(new Pokemon(first_pkmn_id));
 }
+////
+
+//// Destructor
 Pokemon_Party::~Pokemon_Party() {
     std::cout << "Destroying Pokemon Party" << std::endl;
 }
+////
+//
+
+// Getters & Setters
+//// Getters
 std::vector<Pokemon*> Pokemon_Party::getPokemons(){
     return this->pokemons;
 }
-
-void Pokemon_Party::addPokemon(Pokemon pkmn){
-    this->pokemons.push_back(new Pokemon(pkmn));
-}
-void Pokemon_Party::addPokemon(int pkmn_id){
-    this->pokemons.push_back(new Pokemon(pkmn_id));
-}
-void Pokemon_Party::removePokemon(int index){
-    if(index < 0 || index >= pokemons.size()) {
-        std::cout << "Index out of range" << std::endl;
-        return;
-    }
-    delete pokemons.at(index);
-    pokemons.erase(pokemons.begin() + index);
-}
-
 Pokemon* Pokemon_Party::getPokemon(int index){
     if(index < 0 || index >= pokemons.size()) {
         std::cout << "Index out of range" << std::endl;
@@ -35,3 +29,25 @@ Pokemon* Pokemon_Party::getPokemon(int index){
     }
     return pokemons.at(index);
 }
+////
+
+//// Setters
+bool Pokemon_Party::addPokemon(Pokemon pkmn){
+    this->pokemons.push_back(new Pokemon(pkmn));
+    return true;
+}
+bool Pokemon_Party::addPokemon(int pkmn_id){
+    this->pokemons.push_back(new Pokemon(pkmn_id));
+    return true;
+}
+bool Pokemon_Party::removePokemon(int index){
+    if(index < 0 || index >= pokemons.size()) {
+        std::cout << "Index out of range" << std::endl;
+        return false;
+    }
+    delete pokemons.at(index);
+    pokemons.erase(pokemons.begin() + index);
+    return true;
+}
+////
+//
